00f6f872399989dca07a01463646c3528ae328ea
Sin Shimozono <una.veritas@me.com>
2021-11-08 01:18:03 +0900

    fa

--- FiniteAutomata/finiteautomata/__main__.py
+++ FiniteAutomata/finiteautomata/__main__.py
@@ -47,50 +47,52 @@ class FiniteAutomata:
         t += str(self.finals)
         t += ')'
         return t
-
-    def searchtuple(self, singlestate, symbol):
-        left = 0
-        right = len(self.transition)
-        counter = 0
-        while right != left :
-            ix = left + ((right - left)>>1)
-            print(left,right,ix)
-            t = self.transition[ix]
-            if t[:2] >= (state, symbol) :
-                right = ix
-            elif t[:2] < (state, symbol) :
-                left = ix + 1
-            # if counter > 10:
-            #     break
-            # else:
-            #     counter += 1
-        if (state, symbol) == self.transition[left][:2] :
-            collection = set()
-            for i in range(left,len(self.transition)):
-                collection.add(self.transition[i][2])
-        return collection
             
     def transfer(self,state,symbol):
-        if not isinstance(state,set) :
+        if not isinstance(state,set):
             state = set([state])
-            if len(collection) == 1 :
-                return collection.pop()
-            elif len(collection) > 1 :
-                return collection
-        return None
+        nextstate = set()
+        for each in state:
+            left = 0
+            right = len(self.transition)
+            counter = 0
+            while right != left :
+                ix = left + ((right - left)>>1)
+                #print(left,right,ix)
+                t = self.transition[ix]
+                if t[:2] >= (each, symbol) :
+                    right = ix
+                elif t[:2] < (each, symbol) :
+                    left = ix + 1
+                # if counter > 10:
+                #     break
+                # else:
+                #     counter += 1
+            if left >= len(self.transition):
+                #print(left)
+                continue
+            #print('left',left, self.transition[left])
+            if (each, symbol) == self.transition[left][:2] :
+                for ix in range(left,len(self.transition)):
+                    if (each, symbol) == self.transition[ix][:2] :
+                        nextstate.add(self.transition[ix][2])
+            #print('nextstate=',nextstate)
+        if len(nextstate) == 1:
+            nextstate = nextstate.pop()
+        return nextstate
     
     def run(self,inputstr):
         self.reset()
         print('{}'.format(self.currentstate),end='')
         for symb in inputstr:
-            if isinstance(self.currentstate,set):
-                
-            else:
-                self.currentstate = self.transfer(self.currentstate, symb)
-                print(' -{}-> {} '.format(symb, self.currentstate),end='')
-        return self.currentstate in self.finals
+            self.currentstate = self.transfer(self.currentstate, symb)
+            print(' -{}-> {} '.format(symb, self.currentstate),end='')
+        if isinstance(self.currentstate, set) :
+            return bool(self.currentstate.intersection(self.finals))
+        else:
+            return self.currentstate in self.finals
         
 if __name__ == '__main__':
-    fa = FiniteAutomata('01', 'ab', [('0','a','0'),('0','a','1'),('0','a','2'),('0','b','1'),('0','b','2'),('1','a','0'),('1','a','1'),('1','a','2'),('1','b','0'),('1','b','1')],'0','1')
+    fa = FiniteAutomata('0123', 'ab', [('0','a','0'),('0','b','0'),('0','a','1'),('1','a','2'),('2','b','3'),('3','a','3'),('3','b','3')],'0','3')
     print(fa)
-    print(fa.run('aabab'))
+    print(fa.run('aababba'))
